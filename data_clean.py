3eed6ecb7229c54a71273147a2ff8e45284759e0
lucas see <seelcs12@gmail.com>
2024-02-11 18:33:52 -0800

    adding file conversion script

---
+++ data_clean.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import os
+from PIL import Image
+
+parent_folder = os.getcwd()
+input_directory = parent_folder + "\\Data\\no_graffiti\\"
+output_directory = parent_folder + "\\Data-Clean\\no-graffiti\\"
+
+# Change the directory 
+os.chdir(input_directory) 
+
+count = 0
+print(os.listdir())
+for file in os.listdir(): 
+    #try:
+    im = Image.open(file)
+    im.save(output_directory + file[:-4] + str(count) + ".png")
+    count += 1
+    print(count)
+    #except:
+    #    print("File format invalid")
+print("Done")
